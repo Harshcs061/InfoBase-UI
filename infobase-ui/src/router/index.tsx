96d88cc511197c294df354ad09f641ae5e26cbba
MahimaPathak023 <pathakmahima57@gmail.com>
2025-11-30 00:23:44 +0530

    fixed some ui layouts

--- infobase-ui/src/router/index.tsx
+++ infobase-ui/src/router/index.tsx
@@ -3,19 +3,20 @@ import MainLayout from '../layouts/MainLayout';
 import HomePage from '../pages/HomePage';
 import QuestionsPage from '../pages/QuestionsPage';
 import ViewQuestionPage from '../pages/ViewQuestionPage';
-import AskQuestionPage from '../pages/AskQuestionPage';
 import LoginPage from '../pages/LoginPage';
 //import ProtectedRoute from './ProtectedRoute';
 
 const router = createBrowserRouter([
+  {path: "/login",
+   element: <LoginPage />
+  },
   {
     path: "/",
-    element: <MainLayout />,
+    element:<MainLayout />,
     children: [
       { index: true, element: <HomePage /> },
-      { path: "/question", element: <AskQuestionPage /> },
+      { path: "/question", element: <QuestionsPage /> },
       {path: "/questions/:id", element: <ViewQuestionPage />},
-      
     ],
   },
 ]);
